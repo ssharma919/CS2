c17a9b0849289fe540fc226ee6b54f564bd6f058
LightningDog19 <ssharma02@bvsd.org>
2019-08-21 21:50:06 -0600

    In progress with act methods for antFarm

--- src/cs2/antFarm/WorkerAnt.java
+++ src/cs2/antFarm/WorkerAnt.java
@@ -1,10 +1,21 @@
 package cs2.antFarm;
 
+import info.gridworld.actor.Actor;
+
+import javax.xml.stream.Location;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class WorkerAnt extends Ant {
 
     public WorkerAnt() {
         setColor(Color.BLACK);
     }
+
+//    public void act() {
+//        ArrayList<Location> arr = this.getGrid().getEmptyAdjacentLocations(this.getLocation());
+//        int rand = (int) (Math.random()*arr.size());
+//        this.moveTo(arr.get(rand));
+//        this.setDirection(this.getLocation().getDirectionToward(arr.get(rand)));
+//    }
 }
