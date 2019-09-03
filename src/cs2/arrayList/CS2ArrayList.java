99153b4182ede74d4f46a76345acdf280ed41ffb
Suchit Sharma <ssharma02@bvsd.org>
2019-09-03 14:07:53 -0600

    Finished part 1 of CS2List with extensions

--- src/cs2/arrayList/CS2ArrayList.java
+++ src/cs2/arrayList/CS2ArrayList.java
@@ -1,22 +1,39 @@
+
 package cs2.arrayList;
 
 import cs2.CS2List;
 
+/**
+ * This class creates an arrayList with all of its respective methods from an array
+ * @param <E> The type of the objects inside the arrayList
+ */
 public class CS2ArrayList<E> implements CS2List<E> {
 
     private E[] myList;
     private int mySize;
 
+    /**
+     * Creates an instance of CS2List with mySize 0
+     */
     public CS2ArrayList() {
         myList = (E[])(new Object[20]);
         mySize = 0;
     }
 
+    /**
+     * Returns mySize
+     * @return Returns mySize
+     */
     @Override
     public int size() {
         return this.mySize;
     }
 
+    /**
+     * Add an object to the end of the arrayList
+     * @param obj Object you wish to add
+     * @return Returns a boolean dependant on if an object was added or not
+     */
     @Override
     public boolean add(E obj) {
         if (myList.length == mySize) this.expand();
@@ -25,6 +42,11 @@ public class CS2ArrayList<E> implements CS2List<E> {
         return true;
     }
 
+    /**
+     * Adds an object at a specified index
+     * @param index Specified index at which the object is inserted
+     * @param obj Object you wish to add
+     */
     @Override
     public void add(int index, E obj) {
         if (myList.length == mySize || index >= mySize) this.expand();
@@ -36,12 +58,23 @@ public class CS2ArrayList<E> implements CS2List<E> {
         mySize++;
     }
 
+    /**
+     * Returns an object at a specified index
+     * @param index The specified index
+     * @return Object from a specified index
+     */
     @Override
     public E get(int index) {
         if (index < 0 || index >= mySize || mySize == 0) throw new IndexOutOfBoundsException("Attempted to access an index outside of the boundaries");
         return myList[index];
     }
 
+    /**
+     * Changes the object to a new one at a specified index
+     * @param index The specified index
+     * @param obj The new object
+     * @return Returns the previous object
+     */
     @Override
     public E set(int index, E obj) {
         if (index < 0 || index >= mySize || mySize == 0) throw new IndexOutOfBoundsException("Attempted to access an index outside of the boundaries");
@@ -50,6 +83,11 @@ public class CS2ArrayList<E> implements CS2List<E> {
         return temp;
     }
 
+    /**
+     * Removes the object at a specified index
+     * @param index The specified index
+     * @return Returns removed object
+     */
     @Override
     public E remove(int index) {
         if (index < 0 || index >= myList.length || mySize == 0) throw new IndexOutOfBoundsException("Attempted to access an index outside of the boundaries");
@@ -61,6 +99,10 @@ public class CS2ArrayList<E> implements CS2List<E> {
         return temp;
     }
 
+    /**
+     * Returns a formatted array in String
+     * @return The String
+     */
     public String toString() {
         String str = "";
         for (int i = 0; i < mySize; i++) {
@@ -69,6 +111,9 @@ public class CS2ArrayList<E> implements CS2List<E> {
         return str;
     }
 
+    /**
+     * Expands the size of the arrayList if the capacity is reached
+     */
     private void expand() {
         E[] arr = (E[])(new Object[(int)(1.5 * mySize)]);
         for (int i = 0; i < mySize; i++) {
@@ -77,6 +122,11 @@ public class CS2ArrayList<E> implements CS2List<E> {
         myList = arr;
     }
 
+    /**
+     * Returns the first index of a particular object
+     * @param o The particular object
+     * @return The index
+     */
     public int indexOf(Object o) {
         for (int i = 0; i < mySize; i++) {
             if (myList[i].equals(o)) return i;
@@ -84,6 +134,11 @@ public class CS2ArrayList<E> implements CS2List<E> {
         return -1;
     }
 
+    /**
+     * Returns the last index of a particular object
+     * @param o The particular object
+     * @return The index
+     */
     public int lastIndexOf(Object o) {
         for (int i = mySize-1; i >= 0; i--) {
             if (myList[i].equals(o)) return i;
@@ -91,6 +146,9 @@ public class CS2ArrayList<E> implements CS2List<E> {
         return -1;
     }
 
+    /**
+     * Clears the entire arrayList
+     */
     public void clear() {
         mySize = 0;
     }
