63d3209fb75ef8cf6b032e739886510b58b53237
Suchit Sharma <ssharma02@bvsd.org>
2019-09-17 15:15:43 -0600

    Working on LinkedList

--- src/cs2/linkedList/CS2LinkedList.java
+++ src/cs2/linkedList/CS2LinkedList.java
@@ -11,27 +11,90 @@ public class CS2LinkedList<E> implements CS2List<E> {
 
     @Override
     public int size() {
-        return 0;
+        ListNode aNode = head;
+        int count = 0;
+        while (aNode != null) {
+            count++;
+            aNode = aNode.getNext();
+        }
+        return count;
     }
 
     @Override
     public boolean add(E obj) {
-        return false;
+        ListNode ln = new ListNode(obj);
+        ListNode h = head;
+        if (head == null) head = ln;
+        else {
+            while (h.getNext() != null) h = h.getNext();
+            h.setNext(ln);
+        }
+        return true;
+    }
+
+    public String toString() {
+        if (head == null) return "Empty List";
+        String str = "";
+        int count = 0;
+        ListNode aNode = head;
+        while (aNode != null) {
+            str += "[" + count + "]:" + aNode.getValue() + " ";
+            count++;
+            aNode = aNode.getNext();
+        }
+        return str;
     }
 
     @Override
     public void add(int index, E obj) {
+        if (index > this.size()-1 || index < 0) throw new IndexOutOfBoundsException("Index is outside the boundaries");
+        ListNode ln = new ListNode(obj);
+        if (index == 0) {
+            ListNode prevH = head;
+            head = ln;
+            ln.setNext(prevH);
+        } else if (index > 0 && index < this.size()-1){
+            int count = index;
+            ListNode aNode = head;
+            while (count != 1) {
+                aNode = aNode.getNext();
+                count--;
+            }
+            ListNode prevI = aNode.getNext();
+            aNode.setNext(ln);
+            ln.setNext(prevI);
+        } else {
+            ListNode aNode = head;
+            while (aNode.getNext() != null) aNode = aNode.getNext();
+            aNode.setNext(ln);
+        }
 
     }
 
     @Override
     public E get(int index) {
-        return null;
+        if (index > this.size()-1 || index < 0) throw new IndexOutOfBoundsException("Index is outside the boundaries");
+        int count = index;
+        ListNode aNode = head;
+        while (count != 0) {
+            aNode = aNode.getNext();
+            count--;
+        }
+        return aNode.getValue();
     }
 
     @Override
     public E set(int index, E obj) {
-        return null;
+        if (index > this.size()-1 || index < 0) throw new IndexOutOfBoundsException("Index is outside the boundaries");
+        int count = index;
+        ListNode aNode = head;
+        while (count != 0) {
+            aNode = aNode.getNext();
+            count--;
+        }
+        E prevObj = aNode.getValue();
+        aNode.setValue(obj);
+        return prevObj;
     }
 
     @Override
---
+++ src/cs2/linkedList/TestRunner.java
@@ -0,0 +1,13 @@
+package cs2.linkedList;
+
+public class TestRunner {
+    public static void main(String[] args) {
+        CS2LinkedList<Integer> lst = new CS2LinkedList<Integer> ();
+        System.out.println (lst);
+        lst.add (25);
+        lst.add (50);
+        lst.add (75);
+        lst.add (100);
+        System.out.println (lst);
+    }
+}
