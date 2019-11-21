08a28ac13b982ccc34523204ef3970ff365985e5
Suchit Sharma <ssharma02@bvsd.org>
2019-11-21 15:03:08 -0700

    Finished MorseCode

---
+++ src/cs2/morsecode/MorseRunner.java
@@ -0,0 +1,43 @@
+package cs2.morsecode;
+
+import java.io.File;
+import java.lang.reflect.Array;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class MorseRunner {
+    public static void main(String[] args) {
+        Map<Character, String> map = new TreeMap();
+        try {
+            Scanner sc = new Scanner(new File("MorseCode.txt"));
+            while (sc.hasNext()) {
+                String str = sc.nextLine();
+                map.put(str.charAt(0), str.substring(2));
+
+            }
+            sc.close();
+        }
+        catch (Exception e) {
+            throw new NullPointerException();
+        }
+        do {
+            try {
+                Scanner s = new Scanner(System.in);
+                System.out.println("Enter the phrase you would like to translate: ");
+                String result = "";
+                String str = s.nextLine();
+                str = str.toUpperCase();
+                char[] arr = str.toCharArray();
+                for (Character c : arr) {
+                    if (c == ' ') result += "  ";
+                    else if (map.get(c) == null) throw new NullPointerException("Character is not available");
+                    else result += map.get(c) + " ";
+                }
+                System.out.println(result);
+            } catch(Exception e) {
+                System.out.println(e);
+            }
+        } while (true);
+    }
+}
