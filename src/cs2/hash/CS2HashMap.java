2c6438a864d8fb64ca1f716e2e83d7eaab245849
Suchit Sharma <ssharma02@bvsd.org>
2019-12-05 15:09:19 -0700

    Starting timing stuff on hashMaps

--- src/cs2/hash/CS2HashMap.java
+++ src/cs2/hash/CS2HashMap.java
@@ -1,8 +1,6 @@
 package cs2.hash;
 
-import java.util.ArrayList;
-import java.util.Set;
-import java.util.TreeMap;
+import java.util.*;
 
 public class CS2HashMap<K extends Comparable<K>, V> {
     private ArrayList<TreeMap<K, V>> list;
@@ -23,12 +21,12 @@ public class CS2HashMap<K extends Comparable<K>, V> {
     }
 
     public V get(K key) {
-        int bucket = key.hashCode()%list.size();
+        int bucket = key.hashCode() % list.size();
         return list.get(bucket).get(key);
     }
 
-    public V put (K key, V value) {
-        int bucket = key.hashCode()%list.size();
+    public V put(K key, V value) {
+        int bucket = key.hashCode() % list.size();
         V val = list.get(bucket).get(key);
         list.get(bucket).put(key, value);
         return val;
@@ -38,10 +36,47 @@ public class CS2HashMap<K extends Comparable<K>, V> {
         String str = "";
         for (int i = 0; i < size; i++) {
             Set<K> set = list.get(i).keySet();
-            for (K key: set) {
+            for (K key : set) {
                 str += key + " -> " + list.get(i).get(key) + "; ";
             }
         }
         return str;
+    }
+
+    public boolean containsKey(K key) {
+        int bucket = key.hashCode() % list.size();
+        return list.get(bucket).containsKey(key);
+    }
+
+    public boolean containsValue(V val) {
+        for (int i = 0; i < size; i++) {
+            if (list.get(i).containsValue(val)) return true;
+        }
+        return false;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public Set<K> keySet() {
+        Set<K> set = new HashSet<K>();
+        for (int i = 0; i < size; i++) {
+            set.addAll(list.get(i).keySet());
+        }
+        return set;
+    }
+
+    public V remove(K key) {
+        int bucket = key.hashCode() % list.size();
+        return list.get(bucket).remove(key);
+    }
+
+    public Collection<V> values() {
+        Collection<V> col = new HashSet<V>();
+        for (int i = 0; i < size; i++) {
+            col.addAll(list.get(i).values());
+        }
+        return col;
     }
 }
---
+++ src/cs2/hash/CS2HashMapTimingRunner.java
@@ -0,0 +1,23 @@
+package cs2.hash;
+
+public class CS2HashMapTimingRunner {
+    public static void main(String[] args) {
+        int n = 10000;
+        System.out.println("size\tput\tget");
+        for (int i = 1; i < 2*n; i++) {
+            CS2HashMap<Double, Double> map = new CS2HashMap<>(i);
+            long pStart = System.nanoTime();
+            for (int j = 0; j < n; j++) {
+                map.put(Math.random(), 19.19);
+            }
+            long pEnd = System.nanoTime(); long pTime = pEnd - pStart; long pAvgTime = pTime/n;
+            long gStart = System.nanoTime();
+            for (Double key: map.keySet()) {
+                map.get(key);
+            }
+            long gEnd = System.nanoTime(); long gTime = gEnd - gStart; long gAvgTime = gTime/n;
+
+            System.out.println(i+"\t"+pAvgTime+"\t"+gAvgTime);
+        }
+    }
+}
